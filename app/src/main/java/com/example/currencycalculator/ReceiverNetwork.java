a8dccfb6be9958ec10f7c8b0598898f4c227ae3a
Przemyslaw Strojny <przemyslaw.strojny@gmail.com>
2019-11-30 12:46:59 +0100

    Receiver responsible for checking the Internet connection has been created

---
+++ app/src/main/java/com/example/currencycalculator/ReceiverNetwork.java
@@ -0,0 +1,20 @@
+package com.example.currencycalculator;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import static com.example.currencycalculator.MainActivity.dialog;
+
+public class ReceiverNetwork extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        dialog(netInfo != null && netInfo.isConnected());
+
+    }
+}
