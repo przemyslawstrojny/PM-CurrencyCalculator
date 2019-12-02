57078a901e2274dbc8241c3016bda6390774bcbf
Anna Stelmach <annastelmach9696@gmail.com>
2019-12-02 20:32:53 +0100

    Class calculator modified

--- app/src/main/java/com/example/currencycalculator/Calculator.java
+++ app/src/main/java/com/example/currencycalculator/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public double calculate(double value, Currency from, Currency to){
         return (value*from.getConversion_rate()*from.getAverage_rate()*to.getConversion_rate()/to.getAverage_rate());
     }
+
+    public double calculate2(double quantity,Currency from, Currency to) {
+       return (from.getAverage_rate()/to.getAverage_rate()) *  quantity;
+    }
 }
